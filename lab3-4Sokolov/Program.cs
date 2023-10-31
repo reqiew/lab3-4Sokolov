72b36fe30e3226d3f2d4195ab283d44073aacab0
dobry <dobry@DESKTOP-FBD3PAQ>
2023-10-31 04:26:11 +0300

    Добавьте файлы проекта.

---
+++ lab3-4Sokolov/Program.cs
@@ -0,0 +1,20 @@
+﻿//25 вариант средний уровень
+double y = 0;
+Console.WriteLine("|    x   |   y    |");
+for (double x = -0.5; x <= 4.5; x += 0.2)
+{
+    if (x < 0)
+    {
+        y = Math.Exp(-Math.Cos(2 * x) + 1);
+    }
+    else if ((0 <= x) && (x <= 3))
+    {
+        y = Math.Sqrt(2 + Math.Cos(x - 1));
+    }
+    else if (x > 3.5)
+    {
+        y = 2 * Math.Pow(x, 3);
+    }
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+
+}
